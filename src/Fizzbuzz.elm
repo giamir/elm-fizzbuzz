db4af0d5f8d94bec547e85ed316c38f2a0788d05
Giamir Buoncristiani <giamir@thoughtworks.com>
2017-01-30 23:24:20 +0000

    a simple solution

--- src/Fizzbuzz.elm
+++ src/Fizzbuzz.elm
@@ -3,4 +3,11 @@ module Fizzbuzz exposing (..)
 
 fizzbuzz : Int -> String
 fizzbuzz number =
-    "do nothing"
+    if number % 15 == 0 then
+        "fizzbuzz"
+    else if number % 3 == 0 then
+        "fizz"
+    else if number % 5 == 0 then
+        "buzz"
+    else
+        toString number
--- tests/FizzbuzzSpec.elm
+++ tests/FizzbuzzSpec.elm
@@ -8,9 +8,24 @@ import Expect
 all : Test
 all =
     describe "Fizzbuzz"
-        [ test "it should do something" <|
+        [ test "it should returns fizz when I pass 3 as argument" <|
             \() ->
                 1
                     |> fizzbuzz
-                    |> Expect.equal "do something"
+                    |> Expect.equal "fizz"
+        , test "it should returns buzz when I pass 5 as argument" <|
+            \() ->
+                5
+                    |> fizzbuzz
+                    |> Expect.equal "buzz"
+        , test "it should returns fizzbuzz when I pass 15 as argument" <|
+            \() ->
+                15
+                    |> fizzbuzz
+                    |> Expect.equal "fizzbuzz"
+        , test "it should returns 2 as a string when I pass 2 as argument" <|
+            \() ->
+                2
+                    |> fizzbuzz
+                    |> Expect.equal "2"
         ]
